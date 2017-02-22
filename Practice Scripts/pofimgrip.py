b5a8a59646cff53114ad875762430ef29213c2bc
dennistouchet <admin@dennistouchet.com>
2017-02-22 10:39:26 -0600

    Code now pulling profile id's from home page, then iterating through profiles by id, creating a file with pid_username_age, and then adding photos to the individual folders. Only currently pulls women.

--- Practice Scripts/pofimgrip.py
+++ Practice Scripts/pofimgrip.py
@@ -1,16 +1,34 @@
-import requests, bs4, re, urllib
+import requests, bs4, re, urllib, os
+from time import sleep
 
 pofhome = 'http://www.pof.com'
 pofprofile = 'https://www.pof.com/viewprofile.aspx?profile_id='
 pofSoup = None
 pidregex = re.compile(r'(\d){7,10}')
 liregex = re.compile(r'http:.*\.jpg')
+cwd = os.getcwd()
+
+print('folder count: ' + str(len(list(os.walk(cwd)))))   
 
 def get_page(url):
     global pofSoup
     req = requests.get(url)
     req.raise_for_status()
     pofSoup = bs4.BeautifulSoup(req.text, "lxml")    
+
+def save_images(folder, ils):
+    thisdir = cwd + '/' + folder
+    print(thisdir)
+    os.mkdir(thisdir)
+    for n in range(len(ils)):
+        with open(folder + '/image' + str(n) + '.jpg', 'wb') as handle:
+            response = requests.get(ils[n], stream=True)
+            if not response.ok:
+                print(response)
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
 
 print("Starting PoF Image Account Image Ripper")
 
@@ -23,6 +41,7 @@ links = pofSoup.select('.headline .link')
 pids = []
 
 for i in range(len(links)):
+
     pids = pids + [pidregex.search(str(links[i].get('href'))).group()]
     print(pids[i-1])
     
@@ -31,28 +50,23 @@ pofSoup = None
 print(pofprofile + str(pids[0]))
 
 
-#TODO: Check if Profile Already Exists
-get_page(pofprofile + str(pids[0]))
+#get profile images
+def get_profile(pid):
+    get_page(pofprofile + str(pid))
 
-links = pofSoup.select('.image-thumb-wrap a')
-imglinks = []
-for j in range(len(links)):
-    imglinks = imglinks + [liregex.search(str(links[j].get('href'))).group()]
+    links = pofSoup.select('.image-thumb-wrap a')
+    username = pofSoup.select('#username')[0].getText()
+    age = pofSoup.select('#age')[0].getText()
+    folder = str(pid) + '_' + str(username) + '_' + str(age)
+    print(folder)
+    imglinks = []
+    if int(age) >= 21 and len(links) > 3:
+        for j in range(len(links)):
+            imglinks = imglinks + [liregex.search(str(links[j].get('href'))).group()]    
+        save_images(folder, imglinks)
 
-def save_images(ils):
-    for n in range(len(ils)):
-        with open('pic' + str(n) + '.jpg', 'wb') as handle:
-            response = requests.get(ils[n], stream=True)
-
-            if not response.ok:
-                print(response)
-
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-
-                handle.write(block)
-
-save_images(imglinks)
+for i in range(len(pids)):
+    get_profile(pids[i])
+    sleep(1)
     
 print('done')
